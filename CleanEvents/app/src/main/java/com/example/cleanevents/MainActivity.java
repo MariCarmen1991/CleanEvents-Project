cf6b035f6aeb30aeaecfb104735fac4551f1a37e
Mohamed Bakhouza <mohabakh18@gmail.com>
2021-05-14 12:20:19 +0200

    menu_superior

--- CleanEvents/app/src/main/java/com/example/cleanevents/MainActivity.java
+++ CleanEvents/app/src/main/java/com/example/cleanevents/MainActivity.java
@@ -1,9 +1,17 @@
 package com.example.cleanevents;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +21,47 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.navegation_menu, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.politica_privacidad:
+                Toast.makeText(this, "POLITICA PRIVACIDAD", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.quienes_somos:
+                Toast.makeText(this, "QUIENES SOMOS", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.acerca_de:
+                Toast.makeText(this, "ACERCA DE", Toast.LENGTH_SHORT).show();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navigationView = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectFragment = null;
+            switch (item.getItemId()) {
+                case R.id.home:
+                    Toast.makeText(MainActivity.this, "HOME", Toast.LENGTH_SHORT).show();
+                    return true;
+                /*case R.id.perfil_usurio:
+                    Toast.makeText(MainActivity.this, "PERFIL USUARIO", Toast.LENGTH_SHORT).show();
+                    return true;
+                case R.id.anadir_evento:
+                    Toast.makeText(MainActivity.this, "NUEVO EVENTO", Toast.LENGTH_SHORT).show();
+                    return true;*/
+            }
+            //getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer ,selectFragment).commit();
+            return false;
+        }
+    };
 }
