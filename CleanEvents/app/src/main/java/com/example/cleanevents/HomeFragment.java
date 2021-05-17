eeb5472312206e0535fbd1cca12f510c507c2a30
MARI CARMEN <mc.caam@gmail.com>
2021-05-17 13:53:56 +0200

    Todos los cambios que he hecho
    -

--- CleanEvents/app/src/main/java/com/example/cleanevents/HomeFragment.java
+++ CleanEvents/app/src/main/java/com/example/cleanevents/HomeFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
@@ -11,15 +12,19 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.google.firebase.Timestamp;
+
+import java.util.ArrayList;
+import java.util.Date;
+
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link HomeFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class HomeFragment extends Fragment implements View.OnClickListener {
+public class HomeFragment extends Fragment  {
 
-    Button abrirMapa;
-    Button abrirFiltros;
+
     RecyclerView recyclerView;
 
     // TODO: Rename parameter arguments, choose names that match
@@ -66,28 +71,41 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View rootView = inflater.inflate( R.layout.fragment_home, container, false );
-        abrirMapa = rootView.findViewById(R.id.mapa);
-        abrirMapa.setOnClickListener(this);
-        abrirFiltros = rootView.findViewById(R.id.filtros);
-        abrirFiltros.setOnClickListener(this);
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+
+
+        ArrayList<Evento> eventos = new ArrayList<>();
+        Evento nuevoEvento = new Evento();
+        nuevoEvento.setNombre("LIMPIEMOS LA PLAYA");
+        nuevoEvento.setIdUsuario(2);
+        nuevoEvento.setNumParticipantes(10);
+        nuevoEvento.setPoblacion("LLORET DE MAR");
+
+        eventos.add(nuevoEvento);
+        eventos.add(nuevoEvento);
+        eventos.add(nuevoEvento);
+        eventos.add(nuevoEvento);
+
+        //Cargar recyclerView
+        recyclerView = rootView.findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        EventosAdapter adapter = new EventosAdapter(eventos, getActivity());
+
+        recyclerView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+
+
         return rootView;//inflater.inflate(R.layout.fragment_home, container, false);
+
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.mapa:
-                cargarFragment(new MapsFragment());
-                break;
-            case R.id.filtros:
-                //cargarFragment();
-                break;
-        }
-    }
 
-    private void cargarFragment(Fragment fragment) {
+
+    /*private void cargarFragment(Fragment fragment) {
         FragmentTransaction ft =  getActivity().getSupportFragmentManager().beginTransaction();
         ft.replace(R.id.fragmentContainerView, fragment).addToBackStack(null).commit();
-    }
+    }*/
+
+
 }
